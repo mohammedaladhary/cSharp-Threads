81747ae67618ab706809578ab78efc23192db1b8
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-23 13:25:19 +0400

    threads using task

--- Program.cs
+++ Program.cs
@@ -2,26 +2,61 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int someValue = 10;
+        static bool isLocked = false;
+        static async Task Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(doSomeWork));
-            thread.Start();
+            //Thread thread = new Thread(new ThreadStart(doSomeWork));
+            //thread.IsBackground = true;
+            //thread.Start();
 
-            for (int i = 0; i < 10; i++)
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Main Thread: {0}", i);
+            //    Thread.Sleep(10);
+            //}
+            ////thread.Join();
+            //Console.WriteLine("Main Block");
+
+            //Thread thread1 = new Thread(incrementVariable);
+            //Thread thread2 = new Thread(incrementVariable);
+
+            //thread1.Start();
+            //thread2.Start();
+
+            //thread1.Join();
+            //thread2.Join();
+
+            //Console.WriteLine(someValue);
+            
+            int result = await add(2,3);
+            Console.WriteLine("result is: "+result);
+            
+        }
+        static void incrementVariable()
+        {
+            if (!isLocked)
             {
-                Console.WriteLine("Main Thread: {0}", i);
-                Thread.Sleep(10);
+                isLocked = true;
+                for (int i = 0; i < 1000; i++)
+                {
+                    someValue++;
+                }
+                isLocked = false;
             }
-            thread.Join();
-            Console.WriteLine("Main Block");
         }
         public static void doSomeWork()
         {
-            for(int i = 0;i < 10; i++)
+            for(int i = 0;i < 100; i++)
             {
                 Console.WriteLine("Worker Threads: {0}", i);
                 Thread.Sleep(10);
             }
+        }
+        public static async Task<int> add(int a, int b)
+        {
+            //await Task.Delay(5000);
+            return a + b;
         }
     }
 }
